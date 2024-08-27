3341d0856cd36a2496437ffd61a1e24f66f3bc2f
Charlie Bartels <charlie.bartels@daveramsey.com>
2024-08-27 15:28:32 -0500

    Configure react tailwind and jest

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+          <h1 className='text-orange-500 text-5xl'>Tic-Tac-Toe!</h1>
+          <div className='flex justify-center'>
+              <Square />
+          </div>
+      </div>
+  )
+}
+
+function Square() {
+    return <button className="square">X</button>;
+}
+
+
+export default App
