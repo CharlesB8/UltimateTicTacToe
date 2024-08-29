8c0cb2332bd6ffbde13b657458679b32153f47fd
Charlie Bartels <charlie.bartels@daveramsey.com>
2024-08-29 15:18:31 -0500

    rename test file to tsx

--- src/UltimateTicTacToe.test.tsx
+++ src/UltimateTicTacToe.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react'; // I shouldn't need this, so there is still something wrong with the project config
 import {render} from '@testing-library/react';
 import UltimateTicTacToe from "./UltimateTicTacToe";
 
