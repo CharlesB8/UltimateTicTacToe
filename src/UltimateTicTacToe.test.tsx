3080dd09e278b99c8ea6a6b6f4774cfed711faaa
Charlie Bartels <charlie.bartels@daveramsey.com>
2024-09-05 16:34:15 -0500

    make a winner

--- src/UltimateTicTacToe.test.tsx
+++ src/UltimateTicTacToe.test.tsx
@@ -29,10 +29,23 @@ test('O appears after X has played', () => {
 });
 
 test('A box can only be claimed once', () => {
-   const squares = screen.getAllByTestId('square');
+    const squares = screen.getAllByTestId('square');
 
-   fireEvent.click(squares[0]);
-   expect(squares[0]).toContainHTML('X');
-   fireEvent.click(squares[0]);
-   expect(squares[0]).toContainHTML('X')
+    fireEvent.click(squares[0]);
+    expect(squares[0]).toContainHTML('X');
+    fireEvent.click(squares[0]);
+    expect(squares[0]).toContainHTML('X')
+});
+
+test('Claiming three squares in a row makes you win', () => {
+    const squares = screen.getAllByTestId('square');
+
+    fireEvent.click(squares[0]); // X's turn
+    fireEvent.click(squares[3]); // O's turn
+    fireEvent.click(squares[1]); // X's turn
+    fireEvent.click(squares[4]); // O's turn
+    fireEvent.click(squares[2]); // X's turn
+
+    const status = screen.getByTestId('status')
+    expect(status).toContainHTML('Winner: X')
 });
--- src/UltimateTicTacToe.tsx
+++ src/UltimateTicTacToe.tsx
@@ -6,9 +6,7 @@ function UltimateTicTacToe() {
   return (
       <div>
           <h1 className='text-orange-500 text-5xl'>something</h1>
-          <div className='flex justify-center'>
-              <GameBoard />
-          </div>
+          <GameBoard />
       </div>
   );
 }
@@ -35,7 +33,11 @@ function GameBoard() {
     const [squares, setSquares] = useState(Array(9).fill(null))
 
     function handleClick(i: number) {
+        if (squares[i] || calculateWinner(squares)) {
+            return;
+        }
         const nextSquares = squares.slice();
+
         if (xIsNext) {
             nextSquares[i] = 'X';
         } else {
@@ -45,26 +47,61 @@ function GameBoard() {
         setXIsNext(!xIsNext);
     }
 
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next Player: " + (xIsNext ? "X" : "O")
+    }
+
+
     return (
-        <div className='board'>
-            <div className='flex'>
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+        <>
+        <div className='status' data-testid='status'>{status}</div>
+        <div className='flex justify-center'>
+            <div className='board'>
+                    <div className='flex'>
+                        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+                        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+                        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+                    </div>
+                    <div className='flex'>
+                        <Square value={squares[3]}  onSquareClick={() => handleClick(3)}/>
+                        <Square value={squares[4]}  onSquareClick={() => handleClick(4)}/>
+                        <Square value={squares[5]}  onSquareClick={() => handleClick(5)}/>
+                    </div>
+                    <div className='flex'>
+                        <Square value={squares[6]}  onSquareClick={() => handleClick(6)}/>
+                        <Square value={squares[7]}  onSquareClick={() => handleClick(7)}/>
+                        <Square value={squares[8]}  onSquareClick={() => handleClick(8)}/>
+                    </div>
+                </div>
             </div>
-            <div className='flex'>
-                <Square value={squares[3]}  onSquareClick={() => handleClick(3)}/>
-                <Square value={squares[4]}  onSquareClick={() => handleClick(4)}/>
-                <Square value={squares[5]}  onSquareClick={() => handleClick(5)}/>
-            </div>
-            <div className='flex'>
-                <Square value={squares[6]}  onSquareClick={() => handleClick(6)}/>
-                <Square value={squares[7]}  onSquareClick={() => handleClick(7)}/>
-                <Square value={squares[8]}  onSquareClick={() => handleClick(8)}/>
-            </div>
-        </div>
+        </>
     )
 }
 
 
 export default UltimateTicTacToe
+
+function calculateWinner(squares: Array<number>) {
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < winningCombos.length; i++) {
+        const [a, b, c] = winningCombos[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
